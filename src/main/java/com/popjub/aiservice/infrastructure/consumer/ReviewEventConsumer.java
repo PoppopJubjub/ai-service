d15c11e44a134275cf4d47e6a890b9e76eb1a040
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-12-08 04:40:45 +0900

    feat : AI 검사 로직 구현, confidence .8이상만 blind

--- src/main/java/com/popjub/aiservice/infrastructure/consumer/ReviewEventConsumer.java
+++ src/main/java/com/popjub/aiservice/infrastructure/consumer/ReviewEventConsumer.java
@@ -2,8 +2,13 @@ package com.popjub.aiservice.infrastructure.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import com.popjub.aiservice.application.dto.command.AiCommand;
+import com.popjub.aiservice.application.dto.result.AiResult;
 import com.popjub.aiservice.application.event.ReviewCreateEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.popjub.aiservice.application.service.AiService;
+import com.popjub.aiservice.infrastructure.client.ReviewClient;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ReviewEventConsumer {
 
 	private final ObjectMapper objectMapper;
+	private final AiService aiService;
+	private final ReviewClient reviewClient;
 
 	@KafkaListener(topics = "review.created", groupId = "ai-service-group")
 	public void consume(String message) {
@@ -21,9 +28,18 @@ public class ReviewEventConsumer {
 			ReviewCreateEvent event = objectMapper.readValue(message, ReviewCreateEvent.class);
 			log.info("받은 리뷰 이벤트: reviewId={}, content={}", event.getReviewId(), event.getContent());
 
-			// TODO: AI 검사 로직 호출 예정
+			AiCommand command = new AiCommand(event.getReviewId(), event.getContent());
+			AiResult result = aiService.check(command);
+
+			boolean isBlind = result.isAbusive() && result.confidence() >= 0.80;
+
+			if (isBlind) {
+				log.info("리뷰 {} → BLIND 처리 실행", event.getReviewId());
+				reviewClient.updateBlind(event.getReviewId(), true);
+			} else {
+				log.info("리뷰 {} → 정상(BLIND 아님)", event.getReviewId());
+			}
 			// TODO: DB 저장 예정
-			// TODO: review-service로 blind 업데이트 예정
 
 		} catch (Exception e) {
 			log.error("Kafka message parsing error", e);
